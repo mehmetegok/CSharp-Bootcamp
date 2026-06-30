987492cafb7db4dc0c4c52a5cebd2f67e55bf622
mehmetegok <eposta@adresin.commehmeteming275@gmail.com>
2026-06-30 14:48:20 +0300

    feat: learn the logic and mechanics of for loops

---
+++ For_Loop/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace For_Loop
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            for(int i= 1; i<=10; i++)  //ilk durumda i sayacı tanımlanır, 2. durumda i sayacı 10'dan küçük veya eşit olduğu sürece döngü çalışır,
+                                       //3. durumda i sayacı her döngü sonunda 1 artırılır.
+            {
+                listBox1.Items.Add(i + " " + "Merhaba Ankara");
+            }
+        }
+    }
+}

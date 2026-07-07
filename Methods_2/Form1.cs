306bec28cebe73bc030db27b877ca6d64924d568
mehmetegok <eposta@adresin.commehmeteming275@gmail.com>
2026-07-07 19:32:25 +0300

    feat: learn methods with return values and implement a cube calculator method

---
+++ Methods_2/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Methods_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int Kup(int a)
+        {
+            int sonuc = a * a * a;
+            return sonuc;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = Kup(2).ToString();
+        }
+    }
+}

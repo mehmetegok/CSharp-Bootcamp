fb34ac5ec84471ddc8005f5d08fa9b70e4e3252c
mehmetegok <eposta@adresin.commehmeteming275@gmail.com>
2026-07-10 00:33:49 +0300

    feat: learn return methods with summation and square calculation examples

---
+++ Console_2/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Console_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            islem isl = new islem();
+            int sonuc = isl.topla(5, 10);
+
+            islem isl2 = new islem();
+            int karesonucu = isl2.kare(5);
+        }
+    }
+}
---
+++ Console_2/islem.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Console_2
+{
+    internal class islem
+    {
+        public int topla(int a, int b)
+        {
+            int sonuc = a + b;
+            Console.WriteLine("Sonuç:" + sonuc);
+            return sonuc;
+        }
+
+        public int kare(int deger)
+        {
+            int karesonucu = deger * deger;
+            Console.WriteLine("Sonuç:" + karesonucu);
+            return karesonucu;
+        }
+    }
+
+}
